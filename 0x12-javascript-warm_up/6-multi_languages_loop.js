b61564f205cc9ce500670a450351f21e3d9e1393
fosterinc7 <adenoikiadeniyi0108@gmail.com>
2023-10-01 15:04:54 -0700

    OZ

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const line of lines) {
-  console.log(line);
+let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0, len = myArray.length; i < len; i++) {
+  console.log(myArray[i]);
 }
